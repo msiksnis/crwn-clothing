16e940faf1d4dcdf1bc8ded25aa411e0135729e4
marty <marty@martys-MacBook-Pro.local>
2020-03-28 19:37:03 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import toggleCartHidden from "../../redux/cart/cart.actions";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-item.styles.scss";
 
