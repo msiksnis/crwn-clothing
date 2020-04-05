bb990b57e8443e2fc5b815ca451a5ddbfb33339b
marty <marty@martys-MacBook-Pro.local>
2020-04-05 15:06:54 +0200

    updating stripe integration, favicon icon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_5T0AOD9EmlLDMwDoORVrh3s100pD7GzsOW";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Sucesful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Co."
+      shippingAddress
+      billingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
